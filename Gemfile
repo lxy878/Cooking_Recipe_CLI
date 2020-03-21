6a8443e9732928cc278031fc66f23c95831d3eda
Xiaoyi Liu <lxy878@gmail.com>
2020-03-21 10:41:24 +0000

    init

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in cooking_recipe_cli.gemspec
+gemspec
---
+++ lib/cooking_recipe_cli.rb
@@ -0,0 +1,6 @@
+require "cooking_recipe_cli/version"
+
+module CookingRecipeCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
